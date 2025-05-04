4aafa2faef35fb0a7b1f96ab7985852967ac9a6d
namanpatel6 <patel.naman6@gmail.com>
2025-05-04 11:22:38 -0500

    update middleware to allo admin routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,6 +17,8 @@ export async function middleware(request: NextRequest) {
     path === '/auth/signup' || 
     path === '/api/auth/session' ||
     path.startsWith('/api/auth/') ||
+    path.startsWith('/api/admin/') ||    // Add this line
+  path.startsWith('/api/qstash/') ||   // Add this line
     path.includes('_next') ||  // Next.js assets
     path.includes('favicon.ico');
   
